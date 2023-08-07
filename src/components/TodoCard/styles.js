691b16038dd739b43092b7f2b7bf790e73f18ac5
Luciel Santos <luciel.vitor.bs@gmail.com>
2023-08-07 12:25:16 -0300

    ajustes de layout

--- src/components/TodoCard/styles.js
+++ src/components/TodoCard/styles.js
@@ -7,11 +7,23 @@ export const Container = styled.div`
   padding: .5rem 1rem;
   color: ${white};
   display: flex;
+  align-items: center;
+
+  button {
+    outline: none;
+    border: none;
+    border-radius: .25rem;
+    background-color: white;
+    color: black;
+    padding: .25rem 1rem;
+    cursor: pointer;
+  }
 `
 
 export const Text = styled.span`
   font-weight: 600;
   margin-right: auto;
+  margin-left: 1rem;
 
   text-decoration: ${({ done }) => done ? 'line-through' : 'none'};
 `
--- src/styles/global.js
+++ src/styles/global.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    background-color: #E9E9E9;
+    background-color: #f2f6fa;
     margin: 0;
     padding: 0;
     font-family: 'Open Sans', Arial, sans-serif;
